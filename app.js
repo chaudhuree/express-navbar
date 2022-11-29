25b724985d4e415ba97b00ab3e12eeb69ca8443c
chaudhuree <chaudhuree00@gmail.com>
2022-11-30 04:16:01 +0600

    docs: add midlleware again note: here we have to use / if we use \ then it will not work

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require('express')
 const path=require('path')
 const app=express()
 
+// middleware
+app.use(express.static(path.resolve(__dirname,'./public')))
 app.get('/',(req, res) => {
   res.sendFile(path.resolve(__dirname,'\index.html'))
   console.log(__dirname);
